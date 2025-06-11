573c9f7db98b63a3fe4a2f3c3e1edf986ecf45f6
Rishu Raj <rishuraj7581gmail.com@Rishus-MacBook-Air.local>
2025-06-11 22:52:46 +0530

    Update for June 10

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -7,22 +7,50 @@ const Sidebar = () => {
   const { aToken } = useContext(AdminContext);
 
   return (
-    <div>
+    <div className="min-h-screen bg-white border-r">
       {aToken && (
-        <ul>
-          <NavLink>
+        <ul className="text-[#515151] mt-5">
+          <NavLink
+            className={({ isActive }) =>
+              `flex item-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer  ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+              }`
+            }
+            to={"/admin-dashboard"}
+          >
             <img src={assets.home_icon} alt="" />
             <p>Dashboard</p>
           </NavLink>
-          <NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `flex item-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer  ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+              }`
+            }
+            to={"/all-appointments"}
+          >
             <img src={assets.appointment_icon} alt="" />
             <p>Appointment</p>
           </NavLink>
-          <NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `flex item-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer  ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+              }`
+            }
+            to={"/add-doctor"}
+          >
             <img src={assets.add_icon} alt="" />
             <p>Add Doctors</p>
           </NavLink>
-          <NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `flex item-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer  ${
+                isActive ? "bg-[#F2F3FF] border-r-4 border-primary" : ""
+              }`
+            }
+            to={"/doctor-list"}
+          >
             <img src={assets.people_icon} alt="" />
             <p>Doctors List</p>
           </NavLink>
